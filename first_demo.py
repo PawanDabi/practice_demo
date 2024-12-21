671be26a3fda1ef3bde4948108978eaeb68b629c
PawanDabi <pawandabi30400400@gmail.com>
2024-12-21 19:47:13 +0530

    new class added

--- first_demo.py
+++ first_demo.py
@@ -13,3 +13,23 @@ p.display_firstname()
 print("this is the last one")   
 print("this is another method")
 print("this is for main demo")
+
+class Dog:
+
+    def __init__(self, name, age):  
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        print("bark bark!")
+
+    def doginfo(self):
+        print(self.name + " is " + str(self.age) + " year(s) old.")
+
+ozzy = Dog("Ozzy", 2)
+skippy = Dog("Skippy", 12)
+filou = Dog("Filou", 8)
+
+ozzy.doginfo()
+skippy.doginfo()
+filou.doginfo()
