4e8a18b0af0907036903c97092204c41cfe83d09
PawanDabi <pawandabi30400400@gmail.com>
2024-12-21 17:09:33 +0530

    new code has been added

--- first_demo.py
+++ first_demo.py
@@ -1,1 +1,11 @@
-print("hello pawan")
+class Person:
+    def __init__(self, first_name, last_name):
+        self.__name=first_name
+        self.lastname=last_name
+    
+    def display_firstname(self):
+        print(f"your first name is: {self.__name}")
+        print(f"your last name is: {self.lastname}")
+
+p=Person("pawan ", "dabi")
+p.display_firstname()    
